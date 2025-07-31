63ea9db1063dd3f4121564aec668ef1880fc0a0a
Claireko <daiginger@cycu.org.tw>
2025-07-30 17:48:59 -0700

    render_deploy_4

--- apps/frontend/eslint.config.mjs
+++ apps/frontend/eslint.config.mjs
@@ -9,8 +9,19 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+const isCI = process.env.CI === "true";
+
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Override or add rules here:
+  {
+    rules: {
+      "@next/next/no-html-link-for-pages": isCI ? "error" : "warn",
+      "@typescript-eslint/no-explicit-any": isCI ? "error" : "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
+
